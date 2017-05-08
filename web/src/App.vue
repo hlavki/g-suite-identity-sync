74f7e8a795704083e0c72623edcb2363823b3bdc
Michal Hlavac <m.hlavac@xit.camp>
2017-05-08 23:24:39 +0200

    add signOut menu item

--- web/src/App.vue
+++ web/src/App.vue
@@ -10,8 +10,8 @@
       <md-sidenav class="md-left" ref="sidebar" @open="open()" @close="close()">
         <md-toolbar class="md-dense">
           <!--<div class="md-toolbar-container">
-                  <h3 class="md-title">Menu</h3>
-                </div>-->
+                        <h3 class="md-title">Menu</h3>
+                      </div>-->
           <md-list v-if="$auth.loggedIn" class="md-transparent">
             <md-list-item class="md-avatar-list">
               <md-avatar class="md-large">
@@ -32,7 +32,7 @@
           <md-list-item v-if="!$auth.loggedIn" @click.native="routeTo('/sign-in')">
             <md-icon>
               <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                <path fill="#000000" d="M10,17.25V14H3V10H10V6.75L15.25,12L10,17.25M8,2H17A2,2 0 0,1 19,4V20A2,2 0 0,1 17,22H8A2,2 0 0,1 6,20V16H8V20H17V4H8V8H6V4A2,2 0 0,1 8,2Z" />
+                <path fill="#000000" d="M19,3H5C3.89,3 3,3.89 3,5V9H5V5H19V19H5V15H3V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M10.08,15.58L11.5,17L16.5,12L11.5,7L10.08,8.41L12.67,11H3V13H12.67L10.08,15.58Z" />
               </svg>
             </md-icon>
             <span>Sign In</span>
@@ -44,6 +44,14 @@
           <md-list-item v-if="$auth.loggedIn" @click.native="routeTo('/about')">
             <md-icon>help</md-icon>
             <span>About</span>
+          </md-list-item>
+          <md-list-item v-if="$auth.loggedIn" @click.native="signOut()">
+            <md-icon>
+              <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                <path fill="#000000" d="M14.08,15.59L16.67,13H7V11H16.67L14.08,8.41L15.5,7L20.5,12L15.5,17L14.08,15.59M19,3A2,2 0 0,1 21,5V9.67L19,7.67V5H5V19H19V16.33L21,14.33V19A2,2 0 0,1 19,21H5C3.89,21 3,20.1 3,19V5C3,3.89 3.89,3 5,3H19Z" />
+              </svg>
+            </md-icon>
+            <span>Sign Out</span>
           </md-list-item>
         </md-list>
       </md-sidenav>
@@ -75,6 +83,11 @@ export default {
       console.info('Routing to ' + ref)
       this.$router.push(ref)
       this.$refs.sidebar.toggle()
+    },
+    signOut() {
+      console.info('Signing out...')
+      this.$refs.sidebar.toggle()
+      this.$auth.logout()
     }
   }
 }
