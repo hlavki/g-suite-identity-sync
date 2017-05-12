104ff504b89b5d6de518e476ee9c0cb64e73527d
Michal Hlavac <miso@hlavki.eu>
2017-05-12 22:36:15 +0200

    initial support for gsuite allowed groups

--- services/src/main/java/camp/xit/auth/services/rest/security/GSuiteGroupAuthorizationFilter.java
+++ services/src/main/java/camp/xit/auth/services/rest/security/GSuiteGroupAuthorizationFilter.java
@@ -1,7 +1,12 @@
 package camp.xit.auth.services.rest.security;
 
+import camp.xit.auth.services.config.Configuration;
 import camp.xit.auth.services.google.GSuiteDirectoryService;
+import camp.xit.auth.services.google.model.GroupList;
 import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import javax.annotation.Priority;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
@@ -9,7 +14,10 @@ import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Response;
 import org.apache.cxf.jaxrs.utils.JAXRSUtils;
 import org.apache.cxf.rs.security.oauth2.client.ClientTokenContext;
+import org.apache.cxf.rs.security.oidc.common.IdToken;
 import org.apache.cxf.rs.security.oidc.rp.OidcClientTokenContext;
+import org.cache2k.Cache;
+import org.cache2k.Cache2kBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,21 +25,36 @@ import org.slf4j.LoggerFactory;
 public class GSuiteGroupAuthorizationFilter implements ContainerRequestFilter {
 
     private static final Logger log = LoggerFactory.getLogger(GSuiteGroupAuthorizationFilter.class);
+    private static final String GSUITE_DOMAIN_PROP = "gsuite.domain";
+    private static final String GSUITE_SIGN_UP_GROUPS_PROP = "gsuite.signUp.groups";
     private final GSuiteDirectoryService directoryService;
-    private final String domain;
+    private final Configuration config;
+    private final Cache<String, Set<String>> groupCache;
 
 
-    public GSuiteGroupAuthorizationFilter(GSuiteDirectoryService directoryService, String domain) {
+    public GSuiteGroupAuthorizationFilter(GSuiteDirectoryService directoryService, Configuration config) {
         this.directoryService = directoryService;
-        this.domain = domain;
+        this.config = config;
+        this.groupCache = new Cache2kBuilder<String, Set<String>>() {
+        }.expireAfterWrite(15, TimeUnit.MINUTES)
+                .loader((key) -> {
+                    GroupList list = this.directoryService.getGroups(key);
+                    return list.getGroups().stream().map(g -> g.getEmail()).collect(Collectors.toSet());
+                }).build();
     }
 
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         OidcClientTokenContext ctx = (OidcClientTokenContext) JAXRSUtils.getCurrentMessage().getContent(ClientTokenContext.class);
-        String hdParam = ctx.getIdToken().getStringProperty("hd");
-        if (!domain.equalsIgnoreCase(hdParam)) {
+        IdToken idToken = ctx.getIdToken();
+        String subject = idToken.getSubject();
+        String hdParam = idToken.getStringProperty("hd");
+        Set<String> allowGroups = config.getSet(GSUITE_SIGN_UP_GROUPS_PROP);
+        boolean fromGsuite = config.get(GSUITE_DOMAIN_PROP).equalsIgnoreCase(hdParam);
+        allowGroups.retainAll(groupCache.get(subject));
+        boolean memberOfAllowedGroup = !allowGroups.isEmpty();
+        if (!fromGsuite || memberOfAllowedGroup) {
             log.error("Unauthorized access from {}", hdParam);
             requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
         }
