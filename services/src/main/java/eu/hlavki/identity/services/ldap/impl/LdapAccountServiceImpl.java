b6506b5ef0ab3b3e77e6722a57d58fe62c85ddb7
Michal Hlavac <miso@hlavki.eu>
2017-08-31 22:47:17 +0200

    create org units on configure event

--- services/src/main/java/eu/hlavki/identity/services/ldap/impl/LdapAccountServiceImpl.java
+++ services/src/main/java/eu/hlavki/identity/services/ldap/impl/LdapAccountServiceImpl.java
@@ -16,35 +16,40 @@ import java.util.stream.Collectors;
 import org.osgi.service.event.EventHandler;
 
 public class LdapAccountServiceImpl implements LdapAccountService, EventHandler {
-    
+
     private static final Logger log = LoggerFactory.getLogger(LdapAccountServiceImpl.class);
-    
+
     private static final String GROUP_OCLASS = "groupOfNames";
     private static final String GROUP_MEMBER_ATTR = "member";
     public static final String GROUP_NAME_ATTR = "cn";
     private static final String GROUP_DESC_ATTR = "description";
-    
+
     private final LDAPConnectionPool ldapPool;
     private final AppConfiguration config;
-    
-    
+
+
     public LdapAccountServiceImpl(Configuration config, LDAPConnectionPool ldapPool) {
         this.config = config;
         this.ldapPool = ldapPool;
     }
-    
-    
-    private static void configure() {
+
+
+    private void configure() {
         log.info("Configuring UserLdapService...");
+        try (LDAPConnection conn = ldapPool.getConnection()) {
+            creatOrgUnits(conn);
+        } catch (LDAPException e) {
+            log.error("Can't create org units!", e);
+        }
     }
-    
-    
+
+
     @Override
     public boolean accountExists(String subject) throws LDAPException {
         return getAccountDN(subject) != null;
     }
-    
-    
+
+
     @Override
     public String getAccountDN(String subject) throws LDAPException {
         try (LDAPConnection conn = ldapPool.getConnection()) {
@@ -53,8 +58,8 @@ public class LdapAccountServiceImpl implements LdapAccountService, EventHandler 
             return entry != null ? entry.getDN() : null;
         }
     }
-    
-    
+
+
     @Override
     public AccountInfo getAccountInfo(String subject) throws LDAPException {
         AccountInfo result = null;
@@ -68,8 +73,8 @@ public class LdapAccountServiceImpl implements LdapAccountService, EventHandler 
         }
         return result;
     }
-    
-    
+
+
     @Override
     public List<AccountInfo> getAllAccounts() throws LDAPException {
         try (LDAPConnection conn = ldapPool.getConnection()) {
@@ -78,12 +83,11 @@ public class LdapAccountServiceImpl implements LdapAccountService, EventHandler 
             return searchResult.getSearchEntries().stream().map(entry -> AccountUtil.fromLdap(entry)).collect(Collectors.toList());
         }
     }
-    
-    
+
+
     @Override
     public void createAccount(LdapAccount account) throws LDAPException {
         try (LDAPConnection conn = ldapPool.getConnection()) {
-            DN baseDN = new DN(config.getLdapUserBaseDN());
             String entryDN = AccountUtil.getAccountDN(account.getUsername(), config);
             log.info("Creating user with DN {}", entryDN);
             Entry entry = new Entry(entryDN);
@@ -100,8 +104,8 @@ public class LdapAccountServiceImpl implements LdapAccountService, EventHandler 
             conn.add(entry);
         }
     }
-    
-    
+
+
     @Override
     public void updateAccount(LdapAccount account) throws LDAPException {
         try (LDAPConnection conn = ldapPool.getConnection()) {
@@ -117,16 +121,16 @@ public class LdapAccountServiceImpl implements LdapAccountService, EventHandler 
             conn.modify(new ModifyRequest(entryDN, mods));
         }
     }
-    
-    
+
+
     @Override
     public LdapGroup getGroup(String groupName) throws LDAPException {
         try (LDAPConnection conn = ldapPool.getConnection()) {
             return getGroup(groupName, conn);
         }
     }
-    
-    
+
+
     @Override
     public Set<String> getAllGroupNames() throws LDAPException {
         Set<String> result = new HashSet<>();
@@ -142,8 +146,8 @@ public class LdapAccountServiceImpl implements LdapAccountService, EventHandler 
         }
         return result;
     }
-    
-    
+
+
     @Override
     public LdapGroup createOrUpdateGroup(LdapGroup group) throws LDAPException {
         try (LDAPConnection conn = ldapPool.getConnection()) {
@@ -169,8 +173,8 @@ public class LdapAccountServiceImpl implements LdapAccountService, EventHandler 
             return current;
         }
     }
-    
-    
+
+
     @Override
     public Map<String, LdapGroup> getAccountGroups(String accountDN) throws LDAPException {
         Map<String, LdapGroup> result = new HashMap<>();
@@ -191,8 +195,8 @@ public class LdapAccountServiceImpl implements LdapAccountService, EventHandler 
         }
         return result;
     }
-    
-    
+
+
     @Override
     public void addGroupMember(String accountDN, String groupName) throws LDAPException {
         try (LDAPConnection conn = ldapPool.getConnection()) {
@@ -216,8 +220,8 @@ public class LdapAccountServiceImpl implements LdapAccountService, EventHandler 
             }
         }
     }
-    
-    
+
+
     @Override
     public void removeGroupMember(String accountDN, String groupName) throws LDAPException {
         try (LDAPConnection conn = ldapPool.getConnection()) {
@@ -235,8 +239,8 @@ public class LdapAccountServiceImpl implements LdapAccountService, EventHandler 
             }
         }
     }
-    
-    
+
+
     @Override
     public void removeGroup(String groupName) throws LDAPException {
         try (LDAPConnection conn = ldapPool.getConnection()) {
@@ -272,12 +276,30 @@ public class LdapAccountServiceImpl implements LdapAccountService, EventHandler 
         }
         return result;
     }
-    
-    
+
+
     @Override
     public void handleEvent(Event event) {
         if (Configuration.TOPIC_CHANGE.equals(event.getTopic())) {
             configure();
+        }
+    }
+
+
+    private void creatOrgUnits(LDAPConnection conn) throws LDAPException {
+        String peopleDn = config.getLdapUserBaseDN();
+        if (conn.getEntry(peopleDn) == null) {
+            Entry entry = new Entry(peopleDn);
+            entry.addAttribute("objectClass", "top");
+            entry.addAttribute("objectClass", "organizationalUnit");
+            conn.add(entry);
+        }
+        String groupDn = config.getLdapGroupsBaseDN();
+        if (conn.getEntry(groupDn) == null) {
+            Entry entry = new Entry(groupDn);
+            entry.addAttribute("objectClass", "top");
+            entry.addAttribute("objectClass", "organizationalUnit");
+            conn.add(entry);
         }
     }
 }
