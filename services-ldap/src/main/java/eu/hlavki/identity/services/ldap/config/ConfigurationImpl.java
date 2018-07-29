6cafc22a4961a10572e8085b55b239b5f1d6bb72
Michal Hlavac <miso@hlavki.eu>
2018-07-29 18:52:07 +0200

    rollback test configuration from issue #10

--- services-ldap/src/main/java/eu/hlavki/identity/services/ldap/config/ConfigurationImpl.java
+++ services-ldap/src/main/java/eu/hlavki/identity/services/ldap/config/ConfigurationImpl.java
@@ -17,7 +17,7 @@ public class ConfigurationImpl implements Configuration {
     private static final String LDAP_GROUPS_MEMBER_ATTR_PROP = "ldap.groups.memberAttr";
     private static final String LDAP_GROUPS_MEMBER_ATTR_DEFAULT = "uniqueMember";
     private static final String LDAP_USER_ATTR_PROP = "ldap.users.attr";
-    private static final String LDAP_USER_ATTR_DEFAULT = "cn";
+    private static final String LDAP_USER_ATTR_DEFAULT = "uid";
 
     private static final Logger log = LoggerFactory.getLogger(ConfigurationImpl.class);
     private static final String PID = "eu.hlavki.identity.ldap";
@@ -25,7 +25,6 @@ public class ConfigurationImpl implements Configuration {
     private static final String COLLECTIONS_VALUE_SEPARATOR = "|";
     private org.osgi.service.cm.Configuration osgiConfig;
     private final ConfigurationAdmin cfgAdmin;
-
 
     public ConfigurationImpl(ConfigurationAdmin cfgAdmin) {
         this.cfgAdmin = cfgAdmin;
@@ -36,23 +35,19 @@ public class ConfigurationImpl implements Configuration {
         }
     }
 
-
     public String get(String name) {
         Object value = osgiConfig.getProperties().get(name);
         return value != null ? String.valueOf(value) : null;
     }
-
 
     public String get(String name, String defaultValue) {
         String value = get(name);
         return value != null ? value : defaultValue;
     }
 
-
     public boolean isSet(String name) {
         return get(name) != null;
     }
-
 
     public Long getLong(String name, Long defaultValue) {
         Long result = defaultValue;
@@ -67,16 +62,13 @@ public class ConfigurationImpl implements Configuration {
         return result;
     }
 
-
     public Long getLong(String name) {
         return getLong(name, null);
     }
 
-
     public Integer getInt(String name) {
         return getInt(name, null);
     }
-
 
     public Integer getInt(String name, Integer defaultValue) {
         Integer result = defaultValue;
@@ -91,22 +83,18 @@ public class ConfigurationImpl implements Configuration {
         return result;
     }
 
-
     public Boolean getBoolean(String name, Boolean defaultValue) {
         String strValue = get(name);
         return (strValue != null) ? Boolean.valueOf(strValue) : defaultValue;
     }
 
-
     public Boolean getBoolean(String name) {
         return getBoolean(name, null);
     }
 
-
     public Double getDouble(String key) {
         return getDouble(key, null);
     }
-
 
     public Double getDouble(String key, Double defaultValue) {
         Double result = defaultValue;
@@ -121,7 +109,6 @@ public class ConfigurationImpl implements Configuration {
         return result;
     }
 
-
     public BigDecimal getDecimal(String name, BigDecimal defaultValue) {
         BigDecimal result = defaultValue;
         try {
@@ -135,16 +122,13 @@ public class ConfigurationImpl implements Configuration {
         return result;
     }
 
-
     public BigDecimal getDecimal(String name) {
         return getDecimal(name, null);
     }
 
-
     public Set<String> getSet(String name) {
         return getSet(name, COLLECTIONS_VALUE_SEPARATOR);
     }
-
 
     public Set<String> getSet(String name, String separator) {
         Set<String> result;
@@ -161,11 +145,9 @@ public class ConfigurationImpl implements Configuration {
         return result;
     }
 
-
     public List<List<String>> getListOfList(String name) {
         return getListOfList(name, COLLECTIONS_VALUE_SEPARATOR);
     }
-
 
     public List<List<String>> getListOfList(String name, String separator) {
         List<List<String>> result = new ArrayList<>();
@@ -179,11 +161,9 @@ public class ConfigurationImpl implements Configuration {
         return result;
     }
 
-
     public List<String> getList(String name) {
         return getList(name, COLLECTIONS_VALUE_SEPARATOR);
     }
-
 
     public List<String> getList(String name, String separator) {
         List<String> result;
@@ -200,30 +180,25 @@ public class ConfigurationImpl implements Configuration {
         return result;
     }
 
-
     @Override
     public String getBaseDN() {
         return get(LDAP_BASE_DN_PROP);
     }
-
 
     @Override
     public String getLdapUserBaseDN() {
         return get(LDAP_USERS_BASE_DN_PROP) + "," + getBaseDN();
     }
 
-
     @Override
     public String getLdapGroupsBaseDN() {
         return get(LDAP_GROUPS_BASE_DN_PROP) + "," + getBaseDN();
     }
 
-
     @Override
     public String getLdapGroupsObjectClass() {
         return get(LDAP_GROUPS_OBJECT_CLASS_PROP, LDAP_GROUPS_OBJECT_CLASS_DEFAULT);
     }
-
 
     @Override
     public String getLdapGroupsMemberAttr() {
