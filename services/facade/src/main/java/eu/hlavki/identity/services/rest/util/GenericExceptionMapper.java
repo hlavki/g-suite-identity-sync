87d1eee35da0fda056192d4a9770ddf5632e2438
Michal Hlavac <miso@hlavki.eu>
2019-04-04 11:25:40 +0200

    add generic error handler for JAX-RS

---
+++ services/facade/src/main/java/eu/hlavki/identity/services/rest/util/GenericExceptionMapper.java
@@ -0,0 +1,21 @@
+package eu.hlavki.identity.services.rest.util;
+
+import eu.hlavki.identity.services.rest.model.AppError;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Provider
+public class GenericExceptionMapper implements ExceptionMapper<Exception> {
+
+    private static final Logger log = LoggerFactory.getLogger(GenericExceptionMapper.class);
+
+
+    @Override
+    public Response toResponse(Exception exception) {
+        log.error(null, exception);
+        return AppError.toResponse("SERVER_ERR", exception).build();
+    }
+}
